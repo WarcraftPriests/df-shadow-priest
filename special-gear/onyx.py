15b6c8ecf49b01cf591bb6311e4f1b3c1ddacf0f
Publik <sean.peters86@icloud.com>
2023-03-15 14:37:51 -0500

    setup scripts for 10.0.7

---
+++ special-gear/onyx.py
@@ -0,0 +1,56 @@
+"""creates combinations of stones"""
+from itertools import combinations_with_replacement
+
+stones = {
+    'DBS': 204027,  # Desirous Blood Stone
+    'FLS': 204002,  # Flame Licked Stone
+    'SIS': 204000,  # Stone Infused Stone
+    'ETS': 204001,  # Echoing Thunder Stone
+    'EFS': 204005,  # Entropic Fel Stone
+    'FIS': 204011,  # Freezing Ice Stone
+    'HAS': 204018,  # Humming Arcane Stone
+    'PPS': 204022  # Pestilent Plague Stone
+}
+
+
+def combine_name(combo_string):
+    """transforms FIS-FIS-HAS to FIS2-HAS1"""
+    new_combo = ""
+    entries = combo_string.split("-")
+    first = entries.count(entries[0])
+    second = entries.count(entries[1])
+    third = entries.count(entries[2])
+
+    counted_stones = []
+    new_combo = entries[0] + str(first) + "-"
+    counted_stones.append(entries[0])
+
+    if entries[1] not in counted_stones:
+        new_combo += entries[1] + str(second) + "-"
+        counted_stones.append(entries[1])
+
+    if entries[2] not in counted_stones:
+        new_combo += entries[2] + str(third) + "-"
+
+    new_combo = new_combo[:-1]
+
+    return new_combo
+
+
+combos = list(combinations_with_replacement(stones, 3))
+profilesets = []
+
+for combo in combos:
+    RING_COMBO = ""
+    SIMC_STRING = "finger1=onyx_annulet,id=203460,enchant_id=6556,ilevel=424,gem_id="
+    for stone in combo:
+        RING_COMBO = RING_COMBO + stone + "-"
+        SIMC_STRING += str(stones[stone]) + "/"
+    RING_COMBO = combine_name(RING_COMBO[:-1])
+    SIMC_STRING = SIMC_STRING[:-1]
+
+    profilesets.append(
+        f"profileset.\"Onyx_Annulet_1_{RING_COMBO}_424\"+={SIMC_STRING}")
+
+for profileset in profilesets:
+    print(profileset)
